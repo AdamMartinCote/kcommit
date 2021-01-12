d59227687c5dd18c8ec442c0b2b37f7f07188a1d
Adam <me@adammartincote.com>
2021-01-12 12:34:40 -0500

    a

--- kcommit.sh
+++ kcommit.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-function kcommit() {
-  date=$1
-  time=$2
-  message=$3
+date=$1
+time=$2
+message=$3
 
-  printf -v seconds "%02d" $((RANDOM % 60))
+printf -v seconds "%02d" $((RANDOM % 60))
 
-  DATE="${date}T${time}:${seconds}"
-  GIT_COMMITTER_DATE="${DATE}" git commit --date="${DATE}" -m"${message}"
-}
+DATE="${date}T${time}:${seconds}"
+GIT_COMMITTER_DATE="${DATE}" git commit --date="${DATE}" -m"${message}"
